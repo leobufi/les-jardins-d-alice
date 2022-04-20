e528ab7f142ce807e402e7f54e00519c22a98d71
Léo Bufi <bufi.leo@gmail.com>
2022-04-20 11:15:20 +0200

    CRUD on Rayon Product Formation Jardin

---
+++ app/controllers/jardins_controller.rb
@@ -0,0 +1,52 @@
+class JardinsController < ApplicationController
+
+  def index
+    @jardins = Jardin.all
+  end
+
+  def show
+    @jardin = Jardin.find(params[:id])
+  end
+
+  def new
+    @jardin = Jardin.new
+  end
+
+  def create
+    @jardin = Jardin.new(jardin_params)
+    @jardin.user = current_user
+    if current_user.admin?
+      @jardin.save
+      redirect_to dashboard_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @jardin = Jardin.find(params[:id])
+  end
+
+  def update
+    @jardin = Jardin.find(params[:id])
+    if current_user.admin?
+      @jardin.update(jardin_params)
+      redirect_to dashboard_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @jardin = Jardin.find(params[:id])
+    @jardin.destroy
+    redirect_to dashboard_path
+  end
+
+  private
+
+  def jardin_params
+    params.require(:jardin).permit(:title, :description, :photos_url, :main_photo)
+  end
+
+end
--- app/models/jardin.rb
+++ app/models/jardin.rb
@@ -3,5 +3,5 @@ class Jardin < ApplicationRecord
 
   validates :title, presence: true
   validates :description, presence: true
-  validates :main_photo, uniqueness: true
+  # validates :main_photo, uniqueness: true
 end
