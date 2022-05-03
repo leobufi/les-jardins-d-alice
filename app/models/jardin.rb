053dd06e79fbbf30756cc7a39aa0e3a505ee9db4
Léo Bufi <bufi.leo@gmail.com>
2022-05-03 15:37:29 +0200

    lot of front

--- app/models/jardin.rb
+++ app/models/jardin.rb
@@ -4,4 +4,6 @@ class Jardin < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
   # validates :main_photo, uniqueness: true
+  has_rich_text :description
+
 end
