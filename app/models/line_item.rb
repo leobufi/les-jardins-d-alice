e11fbca3eceddb836eb30df89e432852710fccc9
Léo Bufi <bufi.leo@gmail.com>
2022-05-06 17:47:04 +0200

    promo on cart & orders + css stylin

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -5,10 +5,12 @@ class LineItem < ApplicationRecord
   belongs_to :order, optional: true
 
   def total_price
-    if self.product_id
+    if self.coffret_id
+    self.quantity.to_s.to_i * self.coffret.price.to_s.to_i
+    elsif self.product.discount?
+      self.quantity.to_s.to_i * self.product.price_with_discount.to_s.to_i
+    elsif self.product_id
       self.quantity.to_s.to_i * self.product.price.to_s.to_i
-    elsif self.coffret_id
-      self.quantity.to_s.to_i * self.coffret.price.to_s.to_i
     end
   end
 end
