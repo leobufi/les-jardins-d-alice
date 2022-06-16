a0ee01fd5f2549c64f803f7f9d822f337c3e0905
Léo Bufi <bufi.leo@gmail.com>
2022-06-16 14:41:46 +0200

    redirect to changed on contact controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,10 +8,9 @@ class ContactsController < ApplicationController
     @contact.request = request
     if @contact.deliver
       flash[:notice] = 'Votre message a bien été envoyé! Je vous répondrais dès que possible.'
-      redirect_to root_path
     else
       flash[:error] = 'Impossible d\'envoyer votre message'
-      redirect_to prestation_categories_path
     end
+    redirect_to root_path
   end
 end
