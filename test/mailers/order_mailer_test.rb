e26d44fcd45a995efd4a8f8a138603e411d1b9b2
Léo Bufi <bufi.leo@gmail.com>
2022-11-29 14:27:00 +0100

    some small changes on production.rb & media queries

--- test/mailers/order_mailer_test.rb
+++ test/mailers/order_mailer_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class OrderMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def client_confirmation
+    @order = Order.first
+    mail(
+        to: @order.email,
+        from: "lesjardinsdalice@outlook.fr",
+        subject: "Votre commande aux Jardins d\'Alice a bien été prise en compte"
+      )
+  end
 end
