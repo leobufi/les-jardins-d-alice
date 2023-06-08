01df3671c38a831e6267ca9c4f54a5af3e0da401
Léo Bufi <bufi.leo@gmail.com>
2023-06-08 15:25:13 +0200

    dashboard with tab system

--- app/javascript/controllers/switch-tab_controller.js
+++ app/javascript/controllers/switch-tab_controller.js
@@ -1,10 +1,36 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["tab", "content", "partial"]
+  static targets = [
+    "tab",
+    "subTab",
+    "panel",
+    "content",
+    "partial",
+    "title",
+    "project"
+  ]
 
   connect () {
 
+  }
+
+  activate(event) {
+    event.preventDefault()
+    const tab = event.target
+    this.subTabTargets.forEach((tabTarget) => {
+      // console.log(tab.dataset.tabName)
+      tabTarget.classList.toggle("colored", tabTarget == tab)
+    })
+
+    this.panelTargets.forEach((panel) => {
+      // console.log(panel.dataset.tabName)
+      if (panel.dataset.tabName == tab.dataset.tabName) {
+        panel.classList.toggle("activate")
+      } else {
+        panel.classList.remove("activate")
+      }
+    })
   }
 
   switch(event) {
@@ -30,5 +56,23 @@ export default class extends Controller {
         content.classList.add("d-none")
       }
     });
+  }
+
+  show(event) {
+    event.preventDefault()
+    const title = event.target
+    this.titleTargets.forEach((titleTarget) => {
+      // console.log(title.dataset.tabName)
+      titleTarget.classList.toggle("show", titleTarget == title)
+    })
+
+    this.projectTargets.forEach((project) => {
+      // console.log(project.dataset.tabName)
+      if (project.dataset.tabName == title.dataset.tabName) {
+        project.classList.toggle("show")
+      } else {
+        project.classList.remove("show")
+      }
+    })
   }
 }
