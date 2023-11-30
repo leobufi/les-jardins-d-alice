0c3e580e55821c7eb0645ef18b2c29f249b78204
Léo Bufi <bufi.leo@gmail.com>
2023-11-30 13:21:12 +0100

    updated webpack compression plugin 2

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,9 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require('@rails/webpacker');
+const CompressionPlugin = require('compression-webpack-plugin');
+
 
 // Preventing Babel from transpiling NodeModules packages
 environment.loaders.delete('nodeModules');
+environment.plugins.append('CompressionPlugin', new CompressionPlugin());
+
 module.exports = environment
