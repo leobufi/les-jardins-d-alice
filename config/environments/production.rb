cb7581e4dc4c10801dc26ec744ae4f90cc8500a5
Leo Bufi <bufi.leo@gmail.com>
2024-03-26 14:25:07 +0100

    action mailer configured in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,20 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "les_jardins_d_alice_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer_default_url_options = { host: 'https://www.jardinsdalice.com' }
+  Rails.application.routes.default_url_options[:host] = 'https://www.jardinsdalice.com'
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default charset: 'utf-8'
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    authentication: 'plain',
+    user_name: ENV['GMAIL_USERNAME'],
+    password: ENV['GMAIL_PASSWORD']
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
